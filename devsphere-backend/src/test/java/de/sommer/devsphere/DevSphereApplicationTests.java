faa6f9e8577c1447fa821886f7670773b3f69fe4
Tim <151786923+DrSommer20@users.noreply.github.com>
2024-10-31 14:17:06 +0100

    Spring & NG Templates

---
+++ devsphere-backend/src/test/java/de/sommer/devsphere/DevSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sommer.devsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
